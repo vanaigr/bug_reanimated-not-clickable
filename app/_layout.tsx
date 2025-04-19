cfe2d71e419269209273da2c88f6f282a6236f9d
vanaigr <vanaigranov@gmail.com>
2025-04-19 14:35:22 -0500

    1

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,39 +1,159 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import R from 'react'
+import {
+    Modal,
+    View,
+    Text,
+    TouchableOpacity,
+    StyleSheet,
+    ScrollView,
+    TextInput,
+    //Animated
+} from "react-native"
+import Animated, {
+    useSharedValue,
+    withTiming,
+    useAnimatedStyle,
+    runOnJS,
+} from 'react-native-reanimated'
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+export default function() {
+    const [visible, setVisible] = R.useState(false)
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
-
-export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
-
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
-  );
+    return <View style={{ padding: 100 }}>
+        <TouchableOpacity onPress={() => setVisible(true)}>
+            <Text>Open</Text>
+        </TouchableOpacity>
+        <AddModal visible={visible} onClose={() => setVisible(false)}/>
+    </View>
 }
+
+function AddModal({ visible, onClose }: any) {
+    return (
+        <Modal2
+            visible={visible}
+            close={() => onClose(null)}
+        >
+            <View style={{ backgroundColor: 'white' }}>
+                <View>
+                    <View>
+                        <Text>text</Text>
+                    </View>
+                </View>
+                <ScrollView
+                    keyboardShouldPersistTaps='always'
+                    style={{ height: 250 }}
+                >
+                    <View>
+                        <View/><View/><View/><View/><View/>
+                        <View/><View/><View/><View/><View/>
+                        <View/><View/><View/><View/><View/>
+                        <View/><View/><View/><View/><View/>
+                        <View/><View/><View/><View/><View/>
+                    </View>
+                </ScrollView>
+
+                <View>
+                    <TextInput
+                        keyboardType="numeric"
+                        placeholder="0"
+                        value='15'
+                    />
+                </View>
+
+                <View>
+                    <TouchableOpacity onPress={() => onClose(null)}>
+                        <Text>Cancel</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => onClose(null)}>
+                        <Text>Save</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        </Modal2>
+    )
+}
+
+export type Props = R.PropsWithChildren<{
+    visible: boolean
+    close: () => void
+}>
+
+function Modal2({ visible, close, children }: Props) {
+    const animation = useSharedValue(0)
+    const [showModal, setShowModal] = R.useState(visible)
+
+    R.useEffect(() => {
+        if (visible) {
+            animation.value = withTiming(1, { duration: 200 })
+            if(!showModal) setShowModal(true)
+        } else {
+            animation.value = withTiming(0, { duration: 200 }, () => runOnJS(setShowModal)(false))
+        }
+    }, [visible])
+
+    const backdropStyle = useAnimatedStyle(() => ({
+        opacity: animation.value,
+    }))
+
+    const containerStyle = useAnimatedStyle(() => ({
+        transform: [{ translateY: `${100 - (animation.value * 100)}%` }],
+    }))
+
+    return <Modal
+        visible={visible || showModal}
+        onRequestClose={close}
+        transparent={true}
+        statusBarTranslucent={true}
+    >
+        <View style={{ flex: 1 }}>
+            <View style={{ position: 'absolute', inset: 0 }}>
+                <Animated.View
+                    style={[{ flex: 1, backgroundColor: '#00000080' }, backdropStyle]}
+                />
+            </View>
+            <Animated.View
+                style={[{ flex: 1, justifyContent: 'flex-end' }, containerStyle]}
+            >
+                {children}
+            </Animated.View>
+        </View>
+    </Modal>
+}
+
+    /*const off = R.useRef(new Animated.Value(1000)).current
+    const [showModal, setShowModal] = R.useState(visible)
+
+    R.useEffect(() => {
+        if (visible) {
+            Animated.timing(
+                off,
+                { toValue: 0, duration: 200, useNativeDriver: true }
+            ).start()
+            if(!showModal) setShowModal(true)
+        } else {
+            Animated.timing(
+                off,
+                { toValue: 1000, duration: 200, useNativeDriver: true }
+            ).start(() => setShowModal(false))
+        }
+    }, [visible])
+
+    return <Modal
+        visible={visible || showModal}
+        onRequestClose={close}
+        transparent={true}
+        statusBarTranslucent={true}
+    >
+        <View style={{ flex: 1 }}>
+            <View style={{ position: 'absolute', inset: 0 }}>
+                <Animated.View
+                    style={[{ flex: 1, backgroundColor: '#00000080' }, { opacity: off }]}
+                />
+            </View>
+            <Animated.View
+                style={[{ flex: 1, justifyContent: 'flex-end' }, { transform: [{ translateY: off }] }]}
+            >
+                {children}
+            </Animated.View>
+        </View>
+    </Modal>*/
